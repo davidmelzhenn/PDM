c4f959f63d7cea1288471e24f6adc7fc1fc67894
David Melz Henn <davidmelzhenn@gmail.com>
2019-08-13 20:18:07 -0300

    Initial commit

---
+++ app/src/main/java/com/m90143/pdm/MainActivity.java
@@ -0,0 +1,23 @@
+package com.m90143.pdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String TAG = "APP";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+}
