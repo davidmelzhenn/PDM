40c3b0a256cf5ddc91343b780f85eeddf33036fe
David <davidmelzhenn@gmail.com>
2019-09-03 15:00:31 -0300

    Aula 3

--- app/src/main/java/com/m90143/pdm/Main3Activity.java
+++ app/src/main/java/com/m90143/pdm/Main3Activity.java
@@ -12,48 +12,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Main3Activity extends AppCompatActivity {
 
-    private EditText txtValorInicial, txtAplicacaoMensal, txtTempoAplicacao, txtTaxa;
-    private TextView lblResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-        txtValorInicial = findViewById(R.id.txtValorInicial);
-        txtAplicacaoMensal = findViewById(R.id.txtAplicacaoMensal);
-        txtTempoAplicacao = findViewById(R.id.txtTempoAplicacao);
-        txtTaxa = findViewById(R.id.txtTaxa);
-        lblResultado = findViewById(R.id.lblResultado);
-
     }
 
-    public void calcClick(View view) {
-        Double mValorInicial = Double.parseDouble(txtValorInicial.getText().toString());
-        Double mAplicacaoMensal = Double.parseDouble(txtAplicacaoMensal.getText().toString());
-        Integer mTempoAplicacao = Integer.parseInt(txtTempoAplicacao.getText().toString());
-        Double mTaxa = Double.parseDouble(txtTaxa.getText().toString());
-
-        if (mValorInicial == 0) {
-            Toast.makeText(this, "Valor Inicial inválido. Verifique!", Toast.LENGTH_SHORT).show();
-            txtValorInicial.requestFocus();
-        }
-        if (mAplicacaoMensal == 0) {
-            Toast.makeText(this, "Aplicação Mensal inválida. Verifique!", Toast.LENGTH_SHORT).show();
-            txtAplicacaoMensal.requestFocus();
-        }
-        if (mTempoAplicacao == 0) {
-            Toast.makeText(this, "Tempo Aplicação inválido. Verifique!", Toast.LENGTH_SHORT).show();
-            txtTempoAplicacao.requestFocus();
-        }
-        if (mTaxa == 0) {
-            Toast.makeText(this, "Taxa inválida. Verifique!", Toast.LENGTH_SHORT).show();
-            txtTaxa.requestFocus();
-        }
-
-        Double mResultado = (mAplicacaoMensal * (Math.pow(1 + mTaxa, mTempoAplicacao) - 1)) / mTaxa;
-        mResultado += mValorInicial;
-        lblResultado.setText(String.format("Resultado R$: %.2f", mResultado));
-
+    public void poupancaClick(View view) {
+        Intent intent = new Intent(this, PoupancaActivity.class);
+        startActivity(intent);
     }
+
+    public void infoClick(View view) {
+        Intent intent = new Intent(this, PoupancaActivity.class);
+        startActivity(intent);
+    }
+
+    public void quizClick(View view) {
+        Intent intent = new Intent(this, PoupancaActivity.class);
+        startActivity(intent);
+    }
+
+
 }
